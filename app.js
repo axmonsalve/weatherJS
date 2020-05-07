41d232e8dbd985387fc17b536571d4884540c8eb
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-05-07 13:26:49 -0400

    Save location to localstorage

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+//Init storage 
+const storage = new Storage();
+//Get store location data
+const weatherLocation = storage.getLocationData();
 //Init weather object
+const weather = new Weather(weatherLocation.city); //, 'cl'
+//Init UI object
 const ui = new UI();
-const weather = new Weather('santiago', 'cl');
 
+//Get weather  on DOM loaded
 document.addEventListener('DOMContentLoaded', getWeather);
+
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+  //Change location
+  weather.changeLocation(city);
+
+  //set Location and localstorage
+  storage.setLocationData(city);
+
+  //Get the weather again and fetch it
+  getWeather();
+
+  //Close modal
+  $('#locModal').modal('hide');
+});
 
 function getWeather() {
   weather.getWeather()
